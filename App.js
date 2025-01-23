877fc4b2ec234b42cefed093f0cdd729ed4517d9
vamshi0897 <gvamshibtech@gmail.com>
2025-01-23 06:34:49 +0530

    react basic code

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * create server
+ * HMR - Hot Module Replacement
+ * File Watchers Alogithm
+ * bundling
+ * minify
+ * cleaning our code
+ * dev andproduction build
+ * super fast algorithm
+ * image optimization
+ * caching while development
+ * compatable with order version of browser
+ * Https on dev
+ * port number
+ * consistent Hashing algorithm
+ * Zero config
+ * 
+ */
+
+
+
+
+
+import React from 'react';
+
+import ReactDOM from'react-dom/client';
+
+const heading = React.createElement('h1', {}, 'Namaste Everyone');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading);
